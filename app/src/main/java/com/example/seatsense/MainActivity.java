3ecd6404f4f707826731a3377c6cdca2bb9d252c
NayanVR <nvradadiya281@gmail.com>
2024-10-16 02:12:36 +0530

    WebSockets Implemented

--- app/src/main/java/com/example/seatsense/MainActivity.java
+++ app/src/main/java/com/example/seatsense/MainActivity.java
@@ -25,14 +25,6 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         String token = preferences.getString(TOKEN_KEY, null);
 
-        //temporary token
-//    if (token == null) {
-//        SharedPreferences.Editor editor = preferences.edit();
-//        editor.putString(TOKEN_KEY, "dummy_token");  // Add a dummy token
-//        editor.apply();
-//        token = "dummy_token";
-//    }
-
         if (token == null) {
             // No token, redirect to login screen
             Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
@@ -40,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             finish();
         } else {
             // Token exists, redirect to home screen
-            Intent homeIntent = new Intent(MainActivity.this, OccupancyActivity.class);
+            Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
             startActivity(homeIntent);
             finish();
         }
--- app/src/main/java/com/example/seatsense/utils/NetworkUtils.java
+++ app/src/main/java/com/example/seatsense/utils/NetworkUtils.java
@@ -1,8 +1,9 @@
 package com.example.seatsense.utils;
 
 public class NetworkUtils {
-
-    public static final String BASE_URL = "http://192.168.237.2:8000";
+    public static final String SERVER_ADDRESS  = "192.168.237.2:8000";
+    public static final String BASE_URL = "http://" + SERVER_ADDRESS;
+    public static final String BASE_WEBSOCKET_URL = "ws://" + SERVER_ADDRESS;
 
     public static class ErrorResponse {
         private String detail;
